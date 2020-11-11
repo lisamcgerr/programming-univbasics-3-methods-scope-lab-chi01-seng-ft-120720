20cfcd77b91621711195ce35ff5e0b966194d87d
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-11 21:06:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,36 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
+  phrase
 end
 
+phrase
+
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+  status
 end
+
+status
+
+def link
+  danger = "It's Dangerous To Go Alone! Take This."
+  puts danger 
+  danger
+end
+
+link
+
+def all_phrases
+  
+
+
+
+
+  
+    
+
+
+
+
